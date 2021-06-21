b8d82bc0ae642a342e80d8c8457add47c5220701
darkflame <69241628+RishavV123@users.noreply.github.com>
2021-06-21 08:01:15 +0530

    Create install.sh

---
+++ resources/blue/install.sh
@@ -0,0 +1,26 @@
+php /var/www/pterodactyl/artisan down
+cd /var/www/pterodactyl/resources/scripts
+rm -rf user.css
+echo "@import url('https://oreokitten.github.io/1.4/dark-n-purple/user.css')" >> /var/www/pterodactyl/resources/scripts/user.css
+rm -rf index.tsx
+wget https://raw.githubusercontent.com/DeveloperNeon/ptero-1.0-theme-install/main/resources/index.tsx
+cd ../..
+php artisan view:clear
+php artisan cache:clear
+clear
+echo "User panel theme has been added."
+echo "Installing admin theme..."
+cd /var/www/pterodactyl/resources/views/layouts/
+rm -rf admin.blade.php
+wget https://raw.githubusercontent.com/RishavV123/ptero-1.0-theme-install/main/resources/dark-n-purple/admin.blade.php
+npm install yarn -g
+yarn install
+yarn add @emotion/react
+rm -rf /var/www/pterodactyl/resources/scripts/hoc/requireServerPermission.tsx
+yarn build:production
+cd ../../..
+php /var/www/pterodactyl/artisan up
+php /var/www/pterodactyl/artisan view:clear
+php /var/www/pterodactyl/artisan cache:clear
+clear
+echo "Admin theme install complete, Make sure to shift + refresh your page!"
